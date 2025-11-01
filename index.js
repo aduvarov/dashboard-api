fc5a02c73d583c55cc16d7d3a80a0378fee64f12
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-11-01 17:18:18 +0500

    ADD: Промежуточные обработчики

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const host = '127.0.0.1'
 const port = 8000
 const app = express()
 
+app.use((req, res, next) => {
+    console.log('Время ', Date.now())
+    next()
+})
+
 app.get('/hello', (req, res) => {
-    res.send('Привет')
+    throw new Error('Error!!!')
 })
 
 app.use('/users', userRouter)
+
+app.use((err, req, res, next) => {
+    console.log(err.message)
+    res.status(401).send(err.message)
+})
 
 app.listen(port, () => {
     console.log(`Сервер запущен на ${host}:${port}`)
