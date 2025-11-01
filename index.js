9cc1a5f17b60abd05f992418095d65b058dac85d
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-11-01 14:23:28 +0500

    Маршрутизация

--- index.js
+++ index.js
@@ -4,9 +4,23 @@ const host = '127.0.0.1'
 const port = 8000
 const app = express()
 
-app.get('/hello', (req, res) => {
-    res.send('Привет')
+app.all('/hello', (req, res, next) => {
+    console.log('All')
+    next()
 })
+
+const cb = (req, res, next) => {
+    console.log('CB1')
+    next()
+}
+
+app.route('/user')
+    .get(cb, (req, res) => {
+        res.send('Привет')
+    })
+    .post((req, res) => {
+        res.send('Привет POST!')
+    })
 
 app.listen(port, () => {
     console.log(`Сервер запущен на ${host}:${port}`)
