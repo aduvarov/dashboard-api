a1f639c38da5e2590a3b693ac31cfd8807d6387b
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-11-01 15:39:21 +0500

    ADD: Router

--- index.js
+++ index.js
@@ -1,28 +1,15 @@
 import express from 'express'
+import { userRouter } from './users/users.js'
 
 const host = '127.0.0.1'
 const port = 8000
 const app = express()
 
 app.get('/hello', (req, res) => {
-    // res.status(201).json({ success: true })
-    // res.download('/test.pdf', 'tssst.pdf')
-    // res.redirect(301, 'https://example.com')
-    res.cookie('token', 'asdfasdfasdf', {
-        domain: 'devlab.kz',
-        path: '/',
-        secure: true,
-    })
-    res.location('')
-    res.links({
-        next: 'asdf',
-    })
-    res.clearCookie('token')
-    res.set('Content-Type', 'text/plain')
-    res.type('application/json')
-    res.append('Warning', 666)
     res.send('Привет')
 })
+
+app.use('/users', userRouter)
 
 app.listen(port, () => {
     console.log(`Сервер запущен на ${host}:${port}`)
