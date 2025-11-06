186947f1debdccf09a6ac7d302ac7054330a8e0c
Anatoliy D. Uvarov <a.d.uvarov@gmail.com>
2025-11-06 18:35:53 +0500

    ADD: Eslint и prettier

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,30 @@
-import { Container, ContainerModule } from 'inversify'
-import { App } from './app'
-import { ExeptionFilter } from './errors/exeption.filter'
-import { LoggerService } from './logger/logger.service'
-import { UsersController } from './users/users.controller'
-import { ILogger } from './logger/logger.interface'
-import { TYPES } from './types'
-import { IExeptionFilter } from './errors/exeption.filter.interface'
+import { Container, ContainerModule } from 'inversify';
+import { App } from './app';
+import { ExeptionFilter } from './errors/exeption.filter';
+import { LoggerService } from './logger/logger.service';
+import { UsersController } from './users/users.controller';
+import { ILogger } from './logger/logger.interface';
+import { TYPES } from './types';
+import { IExeptionFilter } from './errors/exeption.filter.interface';
 
-export const appBindings = new ContainerModule(bind => {
-    bind.bind<ILogger>(TYPES.ILogger).to(LoggerService)
-    bind.bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter)
-    bind.bind<UsersController>(TYPES.UsersController).to(UsersController)
-    bind.bind<App>(TYPES.Application).to(App)
-})
-
-function bootstrap() {
-    const appContainer = new Container()
-    appContainer.load(appBindings)
-    const app = appContainer.get<App>(TYPES.Application)
-    app.init()
-    return { appContainer, app }
+export interface IBootstrapReturn {
+	appContainer: Container;
+	app: App;
 }
 
-export const { app, appContainer } = bootstrap()
+export const appBindings = new ContainerModule((bind) => {
+	bind.bind<ILogger>(TYPES.ILogger).to(LoggerService);
+	bind.bind<IExeptionFilter>(TYPES.ExeptionFilter).to(ExeptionFilter);
+	bind.bind<UsersController>(TYPES.UsersController).to(UsersController);
+	bind.bind<App>(TYPES.Application).to(App);
+});
+
+function bootstrap(): IBootstrapReturn {
+	const appContainer = new Container();
+	appContainer.load(appBindings);
+	const app = appContainer.get<App>(TYPES.Application);
+	app.init();
+	return { appContainer, app };
+}
+
+export const { app, appContainer } = bootstrap();
